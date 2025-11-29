2e5158763b46a3578c6ad710f5d783f080eb5097
Dr Arkaprabha Sau <33415109+arka1985@users.noreply.github.com>
2025-11-30 02:25:15 +0530

    Fix missing App import and add global error handler to index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 // Force rebuild: Diagnostic Mode
 import React from 'react';
 
